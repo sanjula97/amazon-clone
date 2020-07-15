fa80d3f656bc22cd229381330f54bdcf2053960a
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-15 21:40:41 +0530

    Added the banner

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <img src="https://media.cdn.gradconnection.com/Amazon_Banner_01RjYgj.png" 
+            alt="Banner image"
+            className="home__image"/>
+        </div>
+    )
+}
+
+export default Home;
