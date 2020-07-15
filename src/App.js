277b9faf9547355e4c4550739d36b76a7ead9bbe
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-15 14:12:02 +0530

    seup react router

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import {BrowserRouter as Router , Switch , Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Sanjula</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout page</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login page</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
