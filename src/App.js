2f9e9bb293c3bac9d12807b06dfd7ff9d421dd4c
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-17 03:17:47 +0530

    Added login functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {BrowserRouter as Router , Switch , Route} from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
+import { useStateValue } from './StateProvider';
+import { auth } from './firebase';
 
 function App() {
+
+  const [{basket},dispatch] = useStateValue();
+
+  //UseEffect hook
+  //code which runs on a given condition
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        //loged in
+        dispatch({
+          type:"SET__USER",
+          user:authUser
+        })
+      }else{
+        //logged out
+        dispatch({
+          type:"SET__USER",
+          user:null
+        })
+      }
+    })
+  }, []);
+
   return (
     <Router>
       <div className="app">
--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import './Login.css'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { useState } from 'react'
+import { auth } from './firebase';
 
 function Login() {
+    const history = useHistory();
+    const [email,setEmail] = useState();
+    const [password,setPassword] = useState();
 
-    const login = e => {
-        e.preventDefault();
+    const login = (event) => {
+        event.preventDefault();
         //do the login
+        auth.signInWithEmailAndPassword(email,password)
+        .then((auth) => {
+            //loged in,redirect to home page
+            history.push("/");
+        })
+        .catch((e) => alert(e.message));
+    }
+
+    const register = (event) => {
+        event.preventDefault();
+        //register
+        auth.createUserWithEmailAndPassword(email,password)
+        .then(auth => {
+            //create user,login,redirect to the home page
+            history.push("/");
+        })
+        .catch((e) => alert(e.message));
     }
 
     return (
@@ -22,10 +44,16 @@ function Login() {
                <h1>Sign in</h1>
                <form>
                    <h5>E-mail</h5>
-                   <input type="email"/>
+                   <input 
+                        value={email} 
+                        onChange={event => setEmail(event.target.value)} 
+                        type="email"/>
                    <h5>Password</h5>
-                   <input type="password"/>
-                   <button onClick={login} type="submit" className="login__signInButton">Sign in</button>
+                   <input 
+                        value={password} 
+                        onChange={event => setPassword(event.target.value)} 
+                        type="password"/>
+                   <button onClick={login}  type="submit" className="login__signInButton">Sign in</button>
                </form>
 
                <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
