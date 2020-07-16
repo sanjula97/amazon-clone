7be8f537ac9e7f5634c18b260b552e9da45f090c
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-16 15:31:44 +0530

    Added subtotal for the checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -9,27 +9,35 @@ function Checkout() {
 
     return (
         <div className="checkout">
-            <img className="checkout__add" src="https://images.moneycontrol.com/static-mcnews/2017/10/Amazon-diwali-offer-banner.jpg" alt="Add"/>
-            
-            {basket?.length === 0 ? (
-                <div>
-                    <h2>Your basket is empty</h2>
-                    <p>You have no items in the basket.To buy click on "Add to basket button" on a product that you like.</p>
-                </div>
-            ) : (
-                <div>
-                    <h2 className="checkout__title">Your shopping basket</h2>
-                    {/* list all of the checkout products */}
-                    {basket.map(item => (
-                        <CheckoutProduct 
-                        id = {item.id}
-                        title = {item.title}
-                        image = {item.image}
-                        price = {item.price}
-                        rating = {item.rating}/>
-                    ))}
+            <div className="checkout__left">
+                <img className="checkout__add" src="https://images.moneycontrol.com/static-mcnews/2017/10/Amazon-diwali-offer-banner.jpg" alt="Add"/>
+                
+                {basket?.length === 0 ? (
+                    <div>
+                        <h2>Your basket is empty</h2>
+                        <p>You have no items in the basket.To buy click on "Add to basket button" on a product that you like.</p>
+                    </div>
+                ) : (
+                    <div>
+                        <h2 className="checkout__title">Your shopping basket</h2>
+                        {/* list all of the checkout products */}
+                        {basket.map(item => (
+                            <CheckoutProduct 
+                            id = {item.id}
+                            title = {item.title}
+                            image = {item.image}
+                            price = {item.price}
+                            rating = {item.rating}/>
+                        ))}
+                    </div>
+                )}
+            </div>
+            {basket.length > 0 && (
+                <div className="checkout__right">
+                    <h1>Right</h1>
                 </div>
             )}
+            
         </div>
     )
 }
