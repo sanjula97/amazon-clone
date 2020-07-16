5effe2169ce39a73f38773aeb6a7f62bec98199e
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-16 14:33:03 +0530

    Remove from the basket funtionality added

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './CheckoutProduct.css'
+import { useStateValue } from './StateProvider';
 
 function CheckoutProduct({id,title,image,price,rating}) {
+
+    const [{basket},dispatch] = useStateValue();
+
+    const removeFromBasket = () => {
+        //remove item from the basket
+        dispatch({
+            type:"REMOVE_FROM_BASKET",
+            id:id
+        });
+    }
+
     return (
         <div className="checkoutProduct">
-           <img src={image} alt=""/> 
+           <img className="checkoutProduct__image" src={image} alt=""/> 
            <div className="checkoutProduct__info">
             <p className="checkoutProduct__title">{title}</p>
             <p className="checkoutProduct__price">
@@ -20,7 +32,7 @@ function CheckoutProduct({id,title,image,price,rating}) {
                         ))
                     }
             </div>
-            <button>Remove from basket</button>
+            <button onClick={removeFromBasket}>Remove from basket</button>
            </div>
         </div>
     )
--- src/reducer.js
+++ src/reducer.js
@@ -4,6 +4,7 @@ export  const initialState = {
 };
 
 const reducer = (state,action) => {
+    console.log(action);
     switch(action.type){
         case 'ADD_TO_BASKET':
             //Logic for adding item to basket
@@ -13,7 +14,17 @@ const reducer = (state,action) => {
             };
         case 'REMOVE_FROM_BASKET':
             //Logicfor removing item from basket
-            return {state};
+            let newBasket = [...state.basket];
+
+            const index = state.basket.findIndex((basketItem) => basketItem.id === action.id);
+
+            if(index >= 0){
+                newBasket.splice(index,1);
+            }else {
+                console.warn(`cant remoce product (id:${action.id}) as its not in basket`);
+            }
+
+            return {...state,basket: newBasket}
         default:
             return state;
     }
